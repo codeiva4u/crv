d1aa0ec8abe9dba2b06601d0add2079fbcd96792
codeiva4u <codeiva@protonmail.com>
2025-02-05 20:53:40 +0530

    cloudstream icons updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,9 +96,9 @@ android {
 
         buildTypes {
             release {
-                isDebuggable = false
-                isMinifyEnabled = false
-                isShrinkResources = false
+                isDebuggable = true
+                isMinifyEnabled = true
+                isShrinkResources = true
                 proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
